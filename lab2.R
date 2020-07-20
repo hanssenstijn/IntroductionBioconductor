f0c46b3c50f2fe845f842d944b66b0188f471dab
hanssenstijn <hanssenstijn@gmail.com>
2020-07-20 20:43:48 +0200

    finished

--- lab2.R
+++ lab2.R
@@ -4,6 +4,7 @@ library(readr)
 library(tibble)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 # Import data
 #-----------------------------------------------------#
@@ -31,12 +32,73 @@ brfss %>%
 # Import data
 #-----------------------------------------------------#
 pdata <- read_tsv("datasets/ALLphenoData.tsv")
+
+# Statistical tests
+#-----------------------------------------------------#
 t.test(age ~ sex, pdata)
 pdata %>% t.test(age ~ sex, data = .)
 pdata %>% t.test(age ~ sex, data = ., var.equal = T)
+# Develop function
+t_test <- function(data, formula, ...) {
+    t.test(formula, data, ...)
+}
+pdata %>% t_test(age ~ sex)
+
 # Import data
 #-----------------------------------------------------#
+pdata_file <- file.choose() 
+count_file <- file.choose()
+pdata <- read_csv(pdata_file)
+counts <- read_csv(count_file)
 
-
-# Import data
+# Check data
 #-----------------------------------------------------#
+pdata <- 
+    pdata %>% 
+    select(Run, cell, dex)
+pdata
+eg <- counts[, 1:6] 
+eg
+
+# Combine data
+#-----------------------------------------------------#
+data <- left_join(pdata, eg)
+data
+
+# Transform data
+#-----------------------------------------------------#
+# Gather to transform the wide-format into long-format
+tbl <- gather(data, "Gene", "Count", -(1:3))
+tbl
+tbl %>%
+    group_by(Run) %>%
+    summarize(lib_size = sum(Count))
+tbl %>%
+    group_by(Gene) %>%
+    summarize(
+        ave_count = mean(Count),
+        ave_log_count = mean(log(1 + Count))
+    )
+counts_tbl <- gather(counts, "Gene", "Count", -Run)
+data_tbl <- left_join(pdata, counts_tbl)
+data_tbl
+data_tbl %>%
+    group_by(Run) %>%
+    summarize(lib_size = sum(Count))
+gene_summaries <-
+    data_tbl %>%
+    group_by(Gene) %>%
+    summarize(
+        ave_count = mean(Count),
+        ave_log_count = mean(log(1 + Count))
+    )
+gene_summaries
+
+# Visualize data
+#-----------------------------------------------------#
+ggplot(gene_summaries, aes(ave_log_count)) +
+    geom_density()
+
+# Session Info
+#-----------------------------------------------------#
+sessionInfo()
