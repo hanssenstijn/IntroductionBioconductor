0c648aec8e2374506f1bdc257ae940fd9a4be490
hanssenstijn <hanssenstijn@gmail.com>
2020-07-21 11:51:17 +0200

    Hspaiens data & Mouse CDS squence; subet & visualize data

--- lab4.R
+++ lab4.R
@@ -7,6 +7,11 @@ rm(list = ls())
 library(Biostrings)
 library(GenomicRanges)
 library(IRanges)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+library(BSgenome.Hsapiens.UCSC.hg38)
 
 # Import data
 #-----------------------------------------------------#
@@ -30,5 +35,51 @@ flank(ir,3)
 gr <- GRanges(c("chr1", "chr1", "chr2"), ir, strand=c("+", "-", "+"))
 gr
 
-# Genomic ranges
+# Hsapiens 
 #-----------------------------------------------------#
+chr14_range = GRanges("chr14", IRanges(1, seqlengths(Hsapiens)["chr14"]))
+chr14_dna <- getSeq(Hsapiens, chr14_range)
+letterFrequency(chr14_dna, "GC", as.prob=TRUE)
+
+# Mouse CDS sequence
+#-----------------------------------------------------#
+url <- "ftp://ftp.ensembl.org/pub/release-92/fasta/mus_musculus/cds/Mus_musculus.GRCm38.cds.all.fa.gz"
+fl <- BiocFileCache::bfcrpath(rnames = url)
+cds <- rtracklayer::import(fl, "fasta")
+# Remove cds
+pred1 <- width(cds) %% 3 == 0
+table(pred1)
+pred2 <- narrow(cds, 1, 3) == "ATG"
+stops <- c("TAA", "TAG", "TGA")
+pred3 <- narrow(cds, width(cds) - 2, width(cds)) %in% stops
+table(pred1 & pred2 & pred3)
+cds <- cds[ pred1 & pred2 & pred3 ]
+
+# Subset & Visualize 
+#-----------------------------------------------------#
+hist(log10(width(cds)))
+# Longest width
+cds[ which.max(width(cds)) ]
+# Check name
+names(cds)[ which.max(width(cds)) ]
+# Check GC content of each cds
+gc <- letterFrequency(cds, "GC", as.prob=TRUE)
+head(gc)
+# Visualize
+hist(gc)
+plot( log10(width(cds)), gc, pch=".")
+
+# Summarize codon usage
+#-----------------------------------------------------#
+AMINO_ACID_CODE
+aa <- translate(cds)
+codon_use <- letterFrequency(aa, names(AMINO_ACID_CODE))
+head(codon_use)
+
+# DNAStringSet
+#-----------------------------------------------------#
+mcols(cds) <- DataFrame(
+    GC = gc[,"G|C"]
+)
+mcols(cds, use.names = FALSE)
+mcols(cds[1:3], use.names = FALSE)
