f62f4b6ff350754408168c464e5546740673f730
hanssenstijn <hanssenstijn@gmail.com>
2020-07-25 12:54:49 +0200

    Count matrix input

--- DESeq2/RNA-seq.R
+++ DESeq2/RNA-seq.R
@@ -8,14 +8,16 @@ rm(list = ls())
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install(c("tximport","tximportData","DESeq2"))
+BiocManager::install(c("tximport","tximportData","DESeq2","tximeta","pasilla"))
 
 library("tximport")
 library("tximportData")
 library("readr")
 library("DESeq2")
+library("tximeta")
+library("pasilla")
 
-# Import data
+# Import data tximport
 #-----------------------------------------------------#
 dir <- system.file("extdata", package="tximportData")
 samples <- read.table(file.path(dir,"samples.txt"), header=TRUE)
@@ -33,3 +35,44 @@ txi <- tximport(files, type="salmon", tx2gene=tx2gene)
 ddsTxi <- DESeqDataSetFromTximport(txi,
                                    colData = samples,
                                    design = ~ condition)
+
+# Import data tximeta
+#-----------------------------------------------------#
+coldata <- samples
+coldata$files <- files
+coldata$names <- coldata$run
+se <- tximeta(coldata)
+ddsTxi <- DESeqDataSet(se, design = ~ condition)
+
+# Count matrix input
+#-----------------------------------------------------#
+pasCts <- system.file("extdata",
+                      "pasilla_gene_counts.tsv",
+                      package="pasilla", mustWork=TRUE)
+pasAnno <- system.file("extdata",
+                       "pasilla_sample_annotation.csv",
+                       package="pasilla", mustWork=TRUE)
+cts <- as.matrix(read.csv(pasCts,sep="\t",row.names="gene_id"))
+coldata <- read.csv(pasAnno, row.names=1)
+coldata <- coldata[,c("condition","type")]
+coldata$condition <- factor(coldata$condition)
+coldata$type <- factor(coldata$type)
+# Count matrix and column data are consistent
+# It is absolutely critical that the columns of the count matrix and
+# the rows of the column data (information about samples) are in the same order.
+head(cts,2)
+coldata
+# Samen column order
+rownames(coldata) <- sub("fb", "", rownames(coldata))
+all(rownames(coldata) %in% colnames(cts))
+all(rownames(coldata) == colnames(cts))
+cts <- cts[, rownames(coldata)]
+all(rownames(coldata) == colnames(cts))
+dds <- DESeqDataSetFromMatrix(countData = cts,
+                              colData = coldata,
+                              design = ~ condition)
+dds
+# Add addtional feature data
+featureData <- data.frame(gene=rownames(cts))
+mcols(dds) <- DataFrame(mcols(dds), featureData)
+mcols(dds)
